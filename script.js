ba00e696413e57ae22f149f751c59ff5710e0e5a
Diogo Vieira <diogovieiradeoliveira@gmail.com>
2020-03-14 23:42:47 -0300

    alteração para funcionar em ambientes externos

--- script.js
+++ script.js
@@ -145,9 +145,9 @@ let outputs = {
   } 
 };
 
-let rnn = new mm.MusicRNN('static/drum_kit_rnn');
+let rnn = new mm.MusicRNN('https://storage.googleapis.com/download.magenta.tensorflow.org/tfjs_checkpoints/music_rnn/drum_kit_rnn');
 
-let vae = new mm.MusicVAE('static/drums_2bar_hikl_small');
+let vae = new mm.MusicVAE('https://storage.googleapis.com/download.magenta.tensorflow.org/tfjs_checkpoints/music_vae/drums_2bar_hikl_small
 
 Promise.all([
 	rnn.initialize(),
